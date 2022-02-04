6778fd43b826595ba29a4c91aaf4a0ea675e00e9
octaviomtz <octaviomtz@hotmail.com>
2022-02-04 08:12:16 +0000

    README updated with perception

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # jax_cellular_automata
 ## Overview
 
-
 ## Results
 ![image_synthesis](figures/image_synthesis.png?raw=true) 
+
+## Perception
+We can have a vectorized version of perception (channel wise convolution with specific filters) using vmap. In this case we apply vmap on each axis and convolve each channel of the input image by our identity, sobel and laplacian filters.
+
+```python
+ident = jnp.array([[0.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,0.0]], dtype=jnp.float32)
+sobel_x = jnp.array([[-1.0,0.0,1.0],[-2.0,0.0,2.0],[-1.0,0.0,1.0]], dtype=jnp.float32)/8
+lap = jnp.array([[1.0,2.0,1.0],[2.0,-12,2.0],[1.0,2.0,1.0]], dtype=jnp.float32)
+kernels = jnp.stack([ident, sobel_x, sobel_x.T, lap], axis=0)
+
+@jax.jit
+def perception(img, kernels): 
+    _perception = vmap(vmap(vmap(conv2d, in_axes=(-1, None)), in_axes=(None,0)), in_axes=(0, None)) 
+    return _perception(img, kernels)
+```
+
+![image_synthesis](figures/perception_jax.png?raw=true) 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#%%
 from cv2 import VideoWriter
 import jax
 import jax.numpy as jnp
@@ -22,115 +21,111 @@ import ml_collections
 from utils import load_emoji, get_living_mask
 from utils import VideoWriter
 
-#%%
 EPOCHS = 2000
 LR_INIT = 1e-3
 BATCH_SIZE=4 #for fast synthesis (but no persistance): BATCH_SIZE=1;x0=seed
 
-#%%
-TARGET_SIZE = 40
-TARGET_EMOJI = "🦎"
-target_img = load_emoji(TARGET_EMOJI)
-target_img = target_img[None,:]
-print(target_img.shape)
-plt.imshow(target_img[0])
+def main():
+    TARGET_SIZE = 40
+    TARGET_EMOJI = "🦎"
+    target_img = load_emoji(TARGET_EMOJI)
+    target_img = target_img[None,:]
+    print(target_img.shape)
+    plt.imshow(target_img[0])
 
-# %%
-pool = jnp.zeros((256,40,40,16))
-pool = pool.at[:,20,20,3:].set(1.0)
-seed = jnp.zeros((1,40,40,16))
-seed = seed.at[:,20,20,3:].set(1.0)
+    pool = jnp.zeros((256,40,40,16))
+    pool = pool.at[:,20,20,3:].set(1.0)
+    seed = jnp.zeros((1,40,40,16))
+    seed = seed.at[:,20,20,3:].set(1.0)
 
-#%%
-class CA3(nn.Module):
-  
-  @nn.compact
-  def __call__(self, img):
-      alive_mask = get_living_mask(img)
-      x = nn.Conv(features=128, kernel_size=(3,3), kernel_init=nn.initializers.glorot_uniform(), bias_init=nn.initializers.zeros, padding='SAME')(img)
-      x = nn.relu(x)
-      x = nn.Conv(features=16, kernel_size=(1,1), kernel_init=nn.initializers.zeros, use_bias=False)(x) 
-      x = img + x
-      x *= alive_mask
-      return x
+    # CNN no perception
+    class CA3(nn.Module):
+    @nn.compact
+    def __call__(self, img):
+        alive_mask = get_living_mask(img)
+        x = nn.Conv(features=128, kernel_size=(3,3), kernel_init=nn.initializers.glorot_uniform(), bias_init=nn.initializers.zeros, padding='SAME')(img)
+        x = nn.relu(x)
+        x = nn.Conv(features=16, kernel_size=(1,1), kernel_init=nn.initializers.zeros, use_bias=False)(x) 
+        x = img + x
+        x *= alive_mask
+        return x
 
-# %%
-@jax.jit
-def apply_model(state, img):
-    """Computes gradients, loss and accuracy for a single batch."""
-    def loss_fn(params, x):
-        def call_ca(_, x): return ca.apply({'params': params}, x)
-        x = lax.fori_loop(0, 50, call_ca, x)
-        loss = jnp.mean(jnp.square(x[...,:4]-target_img))
-        return loss, x
+    # apply model, get the loss and backpropagate
+    @jax.jit
+    def apply_model(state, img):
+        """Computes gradients, loss and accuracy for a single batch."""
+        def loss_fn(params, x):
+            def call_ca(_, x): return ca.apply({'params': params}, x)
+            x = lax.fori_loop(0, 50, call_ca, x)
+            loss = jnp.mean(jnp.square(x[...,:4]-target_img))
+            return loss, x
 
-    grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
-    (loss, x), grads = grad_fn(state.params, img)
-    grads = jax.tree_map(lambda x: x/(jnp.linalg.norm(x)+1e-8), grads)
-    state = state.apply_gradients(grads=grads)
-    return state, grads, loss, x
+        grad_fn = jax.value_and_grad(loss_fn, has_aux=True)
+        (loss, x), grads = grad_fn(state.params, img)
+        grads = jax.tree_map(lambda x: x/(jnp.linalg.norm(x)+1e-8), grads)
+        state = state.apply_gradients(grads=grads)
+        return state, grads, loss, x
 
-#  %%
-key = jax.random.PRNGKey(0)
-ca=CA3()
-params_init = ca.init(key, pool[:1,...])['params']
-state = train_state.TrainState.create(
-    apply_fn = ca.apply,
-    params = params_init,
-    tx = optax.adam(LR_INIT))
-
-# %%
-EPOCHS = 8000
-LR = LR_INIT
-key = random.PRNGKey(0)
-losses = []
-sks0 = random.split(key, EPOCHS)
-for idx_epochs, sk0 in tqdm(enumerate(sks0), total=len(sks0)):
-    
-    idx_batch = random.choice(sk0, jnp.arange(0, len(pool), 1, dtype=int), (1,BATCH_SIZE), replace=False)[0]
-    x0 = pool[idx_batch]
-    x0 = x0.at[:1].set(seed)
-    x0=seed
-    state, grads, loss, x = apply_model(state, x0)
-    losses.append(loss)
-
-    pool = pool.at[idx_batch].set(x)
-
-    if idx_epochs == EPOCHS//2:
-        LR = LR * 0.1
-        state = train_state.TrainState.create(
+    # init params and creat state
+    key = jax.random.PRNGKey(0)
+    ca=CA3()
+    params_init = ca.init(key, pool[:1,...])['params']
+    state = train_state.TrainState.create(
         apply_fn = ca.apply,
-        params = state.params,
-        tx = optax.adam(LR)) 
+        params = params_init,
+        tx = optax.adam(LR_INIT))
 
-# %%
-print(losses[-1])
-plt.semilogy(losses, label=f'epochs = {EPOCHS}')
-plt.ylim([1e-5,0])
-plt.legend()
+    # main training loop
+    EPOCHS = 8000
+    LR = LR_INIT
+    key = random.PRNGKey(0)
+    losses = []
+    sks0 = random.split(key, EPOCHS)
+    for idx_epochs, sk0 in tqdm(enumerate(sks0), total=len(sks0)):
+        
+        idx_batch = random.choice(sk0, jnp.arange(0, len(pool), 1, dtype=int), (1,BATCH_SIZE), replace=False)[0]
+        x0 = pool[idx_batch]
+        x0 = x0.at[:1].set(seed)
+        x0=seed
+        state, grads, loss, x = apply_model(state, x0)
+        losses.append(loss)
 
-#%%
-def to_rgb(x):
-    rgb, alpha = x[...,:3], jnp.clip(x[...,3:4],0,1)
-    return 1-alpha+rgb
+        pool = pool.at[idx_batch].set(x)
 
-# %%
-x = seed
-imgs_syn = []
-with VideoWriter(filename = 'synthesis.mp4') as vid:
-    for i in tqdm(range(100)):
-        # im = jnp.clip(x[0,...,:4],0,1)
-        im = jnp.clip(to_rgb(x)[0],0,1)
-        vid.add(im)
-        imgs_syn.append(im)
-        x = ca.apply({'params': state.params}, x)
+        if idx_epochs == EPOCHS//2:
+            LR = LR * 0.1
+            state = train_state.TrainState.create(
+            apply_fn = ca.apply,
+            params = state.params,
+            tx = optax.adam(LR)) 
 
-# %%
-fig, ax = plt.subplots(4,12, figsize=(24,8))
-for i in range(48):
-    ax.flat[i].imshow(imgs_syn[i])
-    # ax.flat[i].hist(imgs_syn[i].flatten())
-    ax.flat[i].axis('off')
-fig.tight_layout()
-plt.savefig('figures/image_synthesis.png')
-# %%
+    # plot loss
+    print(losses[-1])
+    plt.semilogy(losses, label=f'epochs = {EPOCHS}')
+    # plt.ylim([1e-5,0])
+    plt.legend()
+
+    # make synthesis video
+    def to_rgb(x):
+        rgb, alpha = x[...,:3], jnp.clip(x[...,3:4],0,1)
+        return 1-alpha+rgb
+
+    x = seed
+    imgs_syn = []
+    with VideoWriter(filename = 'synthesis.mp4') as vid:
+        for i in tqdm(range(100)):
+            im = jnp.clip(to_rgb(x)[0],0,1)
+            vid.add(im)
+            imgs_syn.append(im)
+            x = ca.apply({'params': state.params}, x)
+
+    # make synthesis figure
+    fig, ax = plt.subplots(4,12, figsize=(24,8))
+    for i in range(48):
+        ax.flat[i].imshow(imgs_syn[i])
+        ax.flat[i].axis('off')
+    fig.tight_layout()
+    plt.savefig('figures/image_synthesis.png')
+
+if __name__ == "__main__":
+    main()
